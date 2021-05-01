32af921cb4be80409d0c5e0cc86e1b28fdd44f7a
Tarek Ali <tarekali15@outlook.com>
2021-05-01 17:32:41 -0500

    random color + speed on reload

--- src/main.js
+++ src/main.js
@@ -6,8 +6,16 @@ const camera = new THREE.PerspectiveCamera(
     1000
 );
 
+function generateColor() {
+    const hexCode = "0123456789ABCDEF";
+    let result = "#";
+    for (let i = 0; i < 6; i++)
+        result += hexCode[Math.floor(Math.random() * 16)]
+    return result;
+}
+
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0xBB });
+const material = new THREE.MeshBasicMaterial({ color: generateColor() });
 const cube = new THREE.Mesh(geometry, material);
 const renderer = new THREE.WebGLRenderer();
 scene.add(cube);
@@ -16,11 +24,12 @@ camera.position.z = 2;
 
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+rotation = (Math.random() * (0.05 - 0.01))
 
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.x += .05;
-    cube.rotation.y += .05;
+    cube.rotation.x += rotation;
+    cube.rotation.y += rotation;
     renderer.render(scene, camera);
 }
 animate();
